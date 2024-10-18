1cdfd74f6c2fd408a491b272dc0e8213fa2abb17
David Bejarano Llano <sonicorn.db@gmail.com>
2024-10-18 09:11:50 +0200

    Update Punto.php

--- src/Punto.php
+++ src/Punto.php
@@ -1,6 +1,26 @@
 <?php
 
 public class Punto{
-    
+
+    private $x;
+    private $y;
+
+    //constructor
+    public function __construct($x,$y){
+        this->x = $x;
+        this->y = $y;
+    }
+
+    public function getX(){
+        return $this->x;
+    }
+
+    public function getY(){
+        return $this->y;
+    }
+
+    public function Distancia($punto){
+        return $this->$punto.getX()+$punto.getY();
+    }
 }
 ?>
