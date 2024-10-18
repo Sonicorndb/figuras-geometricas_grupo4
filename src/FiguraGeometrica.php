518a663d7814c20d5e610a82b4640221b0799334
David Bejarano Llano <sonicorn.db@gmail.com>
2024-10-18 09:30:52 +0200

    Clase FiguraGeometrica terminada

--- src/FiguraGeometrica.php
+++ src/FiguraGeometrica.php
@@ -1,7 +1,24 @@
 <?php
-include_once 'punto.php'
+include_once 'punto.php';
     abstract class FiguraGeometrica extends Punto{
+        protected $nombre;
 
+        public function __construct($nombre){
+            $this->nombre = $nombre;
+            $this->precio = $precio;
+        }
+
+        public function getNombre(){
+            return $this->nombre;
+        }
+
+        abstract public function Area(){
+
+        }
+
+        abstract public function Perimetro(){
+
+        }
     }
 
 ?>
